689bdd593939deecd0c77dcfa89e5ac9851f2cd7
AbroShahzeb <khizr772@gmail.com>
2025-04-24 14:45:37 +0500

    rendered app router in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { AppRouter } from "./routes/AppRouter";
+
 function App() {
   return (
-    <div className="text-2xl bg-surface font-medium flex items-center justify-center w-full min-h-screen">
-      <h1>Hello world 123</h1>
-    </div>
+    <>
+      <AppRouter />
+    </>
   );
 }
 
