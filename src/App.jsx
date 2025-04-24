0f28eebe0931ab90e55b679f323b3a4283bc266e
AbroShahzeb <khizr772@gmail.com>
2025-04-24 13:47:55 +0500

    Deployment check

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="text-2xl font-medium flex items-center justify-center w-full min-h-screen">
-      <h1>Hello world</h1>
+      <h1>Hello world 123</h1>
     </div>
   );
 }
