a05b6d7c5b707cda7b0a07dc087395fe85d0d1f9
AbroShahzeb <khizr772@gmail.com>
2025-04-24 13:29:57 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-2xl font-medium flex items-center justify-center w-full min-h-screen">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
